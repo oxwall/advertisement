a43a03c5a2e4985640b88cb43f1a030e865a852b
esase <alexermashev@gmail.com>
2016-07-13 12:01:11 +0600

    fix for a base table structure

--- update/10606/update.php
+++ update/10606/update.php
@@ -28,37 +28,10 @@
  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-OW::getDbo()->query("
-   CREATE TABLE IF NOT EXISTS `" . OW_DB_PREFIX . "ads_banner` (
-  `id` int(11) NOT NULL auto_increment,
-  `label` varchar(255) NOT NULL,
-  `code` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1");
-
-OW::getDbo()->query("
-   CREATE TABLE IF NOT EXISTS `" . OW_DB_PREFIX . "ads_banner_location` (
-  `id` int(11) NOT NULL auto_increment,
-  `bannerId` int(11) NOT NULL,
-  `location` char(3) NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `bannerId` (`bannerId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5");
-
-OW::getDbo()->query("
-   CREATE TABLE IF NOT EXISTS `" . OW_DB_PREFIX . "ads_banner_position` (
-  `id` int(11) NOT NULL auto_increment,
-  `bannerId` int(11) NOT NULL,
-  `position` enum('top','sidebar','bottom') NOT NULL,
-  `pluginKey` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `bannerId` (`bannerId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1");
-
 $tblPrefix = OW_DB_PREFIX;
 
-$dbo = OW::getDbo();
-$logger = OW::getLogger();
+$dbo = Updater::getDbo();
+$logger = Updater::getLogger();
 
 // add absent columns (fix for https://github.com/skalfa/workflow/issues/1223)
 try
@@ -92,16 +65,3 @@ catch (Exception $e)
 {
     $logger->addEntry(json_encode($e));
 }
-
-
-$authorization = OW::getAuthorization();
-$groupName = 'ads';
-$authorization->addGroup($groupName);
-$authorization->addAction($groupName, 'hide_ads', true);
-$action = BOL_AuthorizationService::getInstance()->findAction('ads', 'hide_ads');
-BOL_AuthorizationPermissionDao::getInstance()->deleteByActionId($action->getId());
-
-OW::getPluginManager()->addPluginSettingsRouteName('ads', 'ads.admin_settings_index');
-
-$path = OW::getPluginManager()->getPlugin('ads')->getRootDir() . 'langs.zip';
-OW::getLanguage()->importPluginLangs($path, 'ads');
